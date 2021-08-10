36e20c2b03b7e2198afb595bf286ace1a10cb46f
sakai kuniya <k_sakai@waku-2.com>
2021-08-10 17:59:22 +0900

    initial commit

---
+++ android/app/src/main/kotlin/com/example/shotani_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shotani_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:shotani_app/application.dart';
+
+void main() {
+  runApp(MyApp());
+}
